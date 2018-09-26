72119dc8fd22936c3f3bf3fc33d2fd7ab0de627f
jberman5 <jberman5@terpmail.umd.edu>
2018-09-25 20:44:13 -0400

    Fixed error in message service

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -13,10 +13,10 @@ export class MessageService {
   clear() {
     this.messages = [];
   }
-
-  getHero(id: number): Observable<Hero> {
+/*
+  getHero(id: number): Observable<Hero[]> {
   // TODO: send the message _after_ fetching the hero
   this.messageService.add(`HeroService: fetched hero id=${id}`);
   return of(HEROES.find(hero => hero.id === id));
-}
+} */
 }
